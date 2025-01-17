68442734370fd0f0c4490bc1ea40e0aacfa6d8dd
Kolesnikov Evgeniy <evgeniy.kolesnikov@devexperts.com>
2025-01-17 20:17:13 +0300

    final exercises

--- hw-1/HomeWork1.py
+++ hw-1/HomeWork1.py
@@ -1,6 +1,7 @@
 import math
-import np
+import numpy
 
+# Ex 1
 
 def share_bread(N, K):
     # your code here
@@ -8,9 +9,9 @@ def share_bread(N, K):
     y = K - x * N
     return x, y
 
-
-# если в функции всё верно, то после выполнения этой строчки, не должно выскакивать ошибок
 assert share_bread(N=3, K=14) == (4, 2)
+
+# Ex 2
 
 
 def leap_year(year_to_check):
@@ -22,35 +23,54 @@ def leap_year(year_to_check):
 
 assert leap_year(5) == 'YOU SHALL NOT PASS'
 
+# Ex 3
+
 
 def amulet_area(a, b, c):
-    # your code here
     p = (a + b + c) / 2
-
     return math.sqrt(p * (p - a) * (p - b) * (p - c))
 
 
 assert amulet_area(3, 4, 5) == 6
 
+# Ex 4
 
 def cal_euclidean(a, b):
-    ## Your code here
-    return np.linalg.norm(a - b)
+    return numpy.linalg.norm(a - b)
 
 def cal_manhattan(a, b):
-    ## Your code here
-    distance = 0
-    for p_i,q_i in zip(a,b):
-        distance += abs(p_i - q_i)
+    return abs(a - b)
 
-    return distance
 
 def cal_cosine(a, b):
-    ## Your code here
-    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+    return numpy.dot(a, b) / (numpy.linalg.norm(a) * numpy.linalg.norm(b))
 
-a = np.random.randint(-10, 10)
-b = np.random.randint(-10, 10)
+a = numpy.random.randint(-10, 10)
+b = numpy.random.randint(-10, 10)
 print(cal_euclidean(a, b))
 print(cal_manhattan(a, b))
 print(cal_cosine(a, b))
+
+# Ex 5
+
+my_array = numpy.random.rand(100)
+print(numpy.max(my_array), numpy.min(my_array))
+print(my_array)
+
+my_array = numpy.random.randint(low=0, high=51, size=(5, 6))
+selected_column = my_array.argmax(axis=1)
+print('Shape: ',my_array.shape)
+print('Array')
+print(my_array)
+print(selected_column)
+
+def unique_rows(X):
+    rows_tuples = [tuple(row) for row in X]
+    unique_tuples = list(set(rows_tuples))
+    unique_matrix = numpy.array(unique_tuples)
+    return unique_matrix
+
+
+X = numpy.random.randint(4, 6, size=(10,3))
+print(X)
+print(unique_rows(X))
